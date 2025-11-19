aff4434b1c857a142ad4c049b68cf24f0d5d20e3
Alexander Kramer <alexander.kramer@timetoact.de>
2025-11-19 12:33:13 +0100

    :sparkles: initialize db on startup

---
+++ src/main/kotlin/com/github/lxkrmr/blog/BlogConfiguration.kt
@@ -0,0 +1,32 @@
+package com.github.lxkrmr.blog
+
+import org.springframework.boot.ApplicationRunner
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class BlogConfiguration {
+
+    @Bean
+    fun databaseInitializer(
+        userRepository: UserRepository,
+        articleRepository: ArticleRepository
+    ) = ApplicationRunner {
+        val johnDoe = userRepository.save(User("johnDoe", "John", "Doe"))
+
+        articleRepository.save(Article(
+            title = "Title",
+            headline = "This is a headline",
+            content = "This is a content",
+            author = johnDoe
+        ))
+
+        articleRepository.save(Article(
+            title = "Another title",
+            headline = "This is another headline",
+            content = "And even more content.",
+            author = johnDoe
+        ))
+
+    }
+}
